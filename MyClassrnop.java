1799f39426a40f50c58efb2578c463eadd2e4266
Bruhathi <91585301+bruhathisp@users.noreply.github.com>
2023-07-10 13:30:48 +0530

    Create MyClassrnop.java

---
+++ MyClassrnop.java
@@ -0,0 +1,19 @@
+//without parameter with return
+
+
+public class MyClass {
+    public static void main(String args[]) {
+      MyClass o=new MyClass();
+      System.out.print(o.area());
+    
+      
+    }
+    
+      public int area(){
+          int a=2;
+          int b=3;
+          int c=a*b;
+          System.out.print("area ");
+    return c;
+}
+}
