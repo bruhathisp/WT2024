343aa45abb29d06f586acc57df05f94b03fcb871
Bruhathi <91585301+bruhathisp@users.noreply.github.com>
2023-07-10 10:14:00 +0530

    Create MyClasspr.java

---
+++ MyClasspr.java
@@ -0,0 +1,19 @@
+//with parameter with return
+
+
+public class MyClasspr {
+    public static void main(String args[]) {
+      MyClass o=new MyClass();
+      System.out.println(o.area(2,3));
+      
+    }
+    
+      public int area(int a, int b){
+          
+      
+ int c= a*b;
+      System.out.print("area ");
+      return c ;
+    
+}
+}
