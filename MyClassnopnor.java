f300d946875c8da933d3ba61010a09577bc6273f
Bruhathi <91585301+bruhathisp@users.noreply.github.com>
2023-07-10 13:25:38 +0530

    Create MyClassnopnor.java

---
+++ MyClassnopnor.java
@@ -0,0 +1,18 @@
+//without parameter without return
+
+
+public class MyClassnopnor {
+    public static void main(String args[]) {
+      MyClass o=new MyClass();
+      o.area();
+    
+      
+    }
+    
+      public void area(){
+          int a=2;
+          int b=3;
+          System.out.print("area "+  a*b);
+    
+}
+}
