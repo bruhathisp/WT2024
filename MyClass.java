b3ac49b20b345ea3a2706bbbe61849b9fc21a537
Bruhathi <91585301+bruhathisp@users.noreply.github.com>
2023-07-10 09:49:49 +0530

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+//with parameter without return
+
+
+public class MyClass {
+    public static void main(String args[]) {
+      MyClass o=new MyClass();
+      o.area(2,3);
+      
+    }
+    
+      public void area(int a, int b){
+          System.out.print("area "+  a*b);
+    
+}
+}
